44edad6e3650726482b35fa108343c90f6c1e715
Noureldin Elhelw <noureldinlehelw14@gmail.com>
2022-02-28 16:42:58 -0500

    Installed MeepMeep

---
+++ MeepMeep/src/main/java/com/example/android/meepmeep/MeepMeepTesting.java
@@ -0,0 +1,52 @@
+package com.example.android.meepmeep;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TranslationalVelocityConstraint;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.AddTrajectorySequenceCallback;
+import com.noahbres.meepmeep.roadrunner.Constraints;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.DriveShim;
+import com.noahbres.meepmeep.roadrunner.DriveTrainType;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.roadrunner.entity.TrajectorySequenceEntity;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        DriveShim drive = new DriveShim(DriveTrainType.MECANUM, );
+
+        TrajectorySequenceBuilder redWarehoue = new TrajectorySequenceBuilder(new Pose2d(12, -62, Math.toRadians(90)))
+                .splineTo(new Vector2d(2.5, -43), Math.toRadians(125))
+                .waitSeconds(1)
+                .splineToLinearHeading(new Pose2d(12, -62), Math.toRadians(0))
+                .forward(40)
+                .build();
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep).setDimensions(14.5, 18)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(56.9087263114899325, 56.9087263114899325, Math.toRadians(180), Math.toRadians(180), 13.45)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -62, Math.toRadians(90)))
+                                .splineTo(new Vector2d(2.5, -43), Math.toRadians(125))
+                                .waitSeconds(1)
+//                                .splineTo(new Vector2d(2, -45), Math.toRadians(0))
+                                .splineToLinearHeading(new Pose2d(12, -62), Math.toRadians(0))
+                                //.turn(Math.toRadians(-125))
+                                //.waitSeconds(1)
+                                //.forward(40)
+                                .forward(40)
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
